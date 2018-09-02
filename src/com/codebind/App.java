1bbcc8c60c5bf4a240c44435a80e8fc091cd24f0
andrius7407 <andrius.el@gmail.com>
2018-09-02 09:29:55 +0300

    Initial commit

---
+++ src/com/codebind/App.java
@@ -0,0 +1,27 @@
+package com.codebind;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JButton button_msg;
+    private JPanel panelMain;
+
+    public App() {
+        button_msg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Labas rytas!!!");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Lentelė");
+        frame.setContentPane(new App().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
